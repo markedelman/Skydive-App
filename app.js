bb54a3c2ec202d8e40eb5fd65ab7a28a6877e3f2
Mark Edelman <markedelman1987@gmail.com>
2017-05-16 02:53:52 -0400

    modifications made to display pictures and text according to windspeed being greater than or less than 23mph

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
  // AJAX request
  const getDataFromApi = (searchTermCity, searchTermState, callback) => {
      let settings = {
-         url: `http://api.wunderground.com/api/0bb3c0d8c3ed1468/forecast/q/${searchTermState}/${searchTermCity}.json`,
+         url: `http://api.wunderground.com/api/0bb3c0d8c3ed1468/conditions/q/${searchTermState}/${searchTermCity}.json`,
          format: 'json',
          type: 'GET',
          success: callback
      };
-     console.log(settings.url);
      $.ajax(settings);
  };
 
  // Render Results
  const displayWunderGroundData = (data) => {
      let resultElement = '';
-     if (data.forecast) {
-
+     if (data.response) {
          // JSON object traversal
-         console.log(data.forecast);
-         let wunderObject = data.forecast.txt_forecast.forecastday[0].icon;
-         let currentIcon = data.forecast.txt_forecast.forecastday[0].icon_url;
-         let currentForecast = data.forecast.txt_forecast.forecastday[0].fcttext;
+         let windSpeed = data.current_observation.wind_mph;
+         let windGusts = data.current_observation.wind_gust_mph;
+         let windString = data.current_observation.wind_string;
+         let conditionString = data.current_observation.weather;
+         let weatherIcon = data.current_observation.icon_url;
+         let clearToJump = `https://scontent-dft4-2.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/14553153_176777539455544_2049394605073367040_n.jpg`;
+         let clearToDrink = `https://cdn0.vox-cdn.com/thumbor/xScrnYpOjnGQN5lbP_QiE6SGvdQ=/0x69:540x474/1200x900/filters:focal(0x69:540x474)/cdn0.vox-cdn.com/uploads/chorus_image/image/48786051/Bad_Weather_Brewing_credit_FB.0.0.jpg`;
 
-         // Check if wunderground set a raining icon
-         if (wunderObject === "rain" || wunderObject === "tstorms") {
+         if (windSpeed > "23" && windGusts > "23") {
              resultElement +=
                  `<div class="row">
                     <div class="col-6">
                       <div class="box">
-                        <img src=${currentIcon} alt="rain" height="42" width="42">
-                        ${currentForecast}
+                        <img class="clear-to-jump-img" src=${clearToJump} alt="rain" height="42" width="42">
                       </div>
                     </div>
                     <div class="col-6">
                       <div class="box">
-                        IF IT ISN'T ALREADY RAINING ON YOU TODAY, IT WILL BE SOON :)
+                        <span class="clear-to-jump-text">Think you're good to go, bro!</span>
                       </div>
                     </div>
                   </div>`;
@@ -42,13 +41,12 @@
                  `<div class="row">
                     <div class="col-6">
                       <div class="box">
-                        <img src=${currentIcon} alt="sun" height="42" width="42">
-                        ${currentForecast}
+                        <img class="clear-to-drink-img" src=${clearToDrink} alt="sun" height="42" width="42">
                       </div>
                     </div>
                     <div class="col-6">
                       <div class="box">
-                        ITS NOT RAINING ON YOU TODAY....YET :(
+                        <span class="clear-to-drink-text">Better to drink on the ground and wish you were in the sky then to be in sky wishing you were on the ground :)</span>
                       </div>
                     </div>
                   </div>`;
@@ -63,7 +61,6 @@
  const watchSubmit = () => {
      $('.button').click((e) => {
          e.preventDefault();
-         console.log('hello');
          let queryCity = $('.js-query-city').val().replace(/ /g, "_");
          let queryState = $('.js-query-state').val();
          getDataFromApi(queryCity, queryState, displayWunderGroundData);
