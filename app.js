b21e88283c298b9de939bdf94474d7d3874b3fe6
Mark Edelman <markedelman1987@gmail.com>
2017-05-16 01:50:48 -0400

    RainApp is now SkydiveApp and responsive

--- app.js
+++ app.js
@@ -1,52 +1,76 @@
  // AJAX request
-const getDataFromApi = (searchTermCity, searchTermState, callback) => {
-    let settings = {
-        url: `http://api.wunderground.com/api/0bb3c0d8c3ed1468/forecast/q/${searchTermState}/${searchTermCity}.json`,
-        format: 'json',
-        type: 'GET',
-        success: callback
-    };
-    $.ajax(settings);
-};
+ const getDataFromApi = (searchTermCity, searchTermState, callback) => {
+     let settings = {
+         url: `http://api.wunderground.com/api/0bb3c0d8c3ed1468/forecast/q/${searchTermState}/${searchTermCity}.json`,
+         format: 'json',
+         type: 'GET',
+         success: callback
+     };
+     console.log(settings.url);
+     $.ajax(settings);
+ };
 
-// Render Results
-const displayWunderGroundData = (data) => {
-    let resultElement = '';
-    if (data.forecast) {
-        // JSON object traversal
-        let wunderObject = data.forecast.txt_forecast.forecastday[0].icon;
-        let currentIcon = data.forecast.txt_forecast.forecastday[0].icon_url;
-        let currentForecast = data.forecast.txt_forecast.forecastday[0].fcttext;
+ // Render Results
+ const displayWunderGroundData = (data) => {
+     let resultElement = '';
+     if (data.forecast) {
 
-        // Check if wunderground set a raining icon
-        if (wunderObject === "rain" || wunderObject === "tstorms") {
-            resultElement +=
-                `<img src=${currentIcon} alt="rain" height="42" width="42">
-                <br>${currentForecast}
-                <br>IF IT ISN'T ALREADY RAINING ON YOU TODAY, IT WILL BE SOON :)`;
-        } else {
-            resultElement +=
-                `<img src=${currentIcon} alt="sun" height="42" width="42">
-                <br>${currentForecast}
-                <br>ITS NOT RAINING ON YOU TODAY....YET :(`;
-        }
-    } else {
-        alert("Sorry, please check the city and state fields and try again");
-    }
-    $('.js-results').html(resultElement);
-};
+         // JSON object traversal
+         console.log(data.forecast);
+         let wunderObject = data.forecast.txt_forecast.forecastday[0].icon;
+         let currentIcon = data.forecast.txt_forecast.forecastday[0].icon_url;
+         let currentForecast = data.forecast.txt_forecast.forecastday[0].fcttext;
 
-// Event listener for 'submit' button
-const watchSubmit = () => {
-    $('.js-search-form').submit((e) => {
-        e.preventDefault();
-        let queryCity = $('.js-search-form').find('.js-query-city').val().replace(/ /g, "_");
-        let queryState = $('.js-search-form').find('.js-query-state').val();
-        getDataFromApi(queryCity, queryState, displayWunderGroundData);
-    });
-};
+         // Check if wunderground set a raining icon
+         if (wunderObject === "rain" || wunderObject === "tstorms") {
+             resultElement +=
+                 `<div class="row">
+                    <div class="col-6">
+                      <div class="box">
+                        <img src=${currentIcon} alt="rain" height="42" width="42">
+                        ${currentForecast}
+                      </div>
+                    </div>
+                    <div class="col-6">
+                      <div class="box">
+                        IF IT ISN'T ALREADY RAINING ON YOU TODAY, IT WILL BE SOON :)
+                      </div>
+                    </div>
+                  </div>`;
+         } else {
+             resultElement +=
+                 `<div class="row">
+                    <div class="col-6">
+                      <div class="box">
+                        <img src=${currentIcon} alt="sun" height="42" width="42">
+                        ${currentForecast}
+                      </div>
+                    </div>
+                    <div class="col-6">
+                      <div class="box">
+                        ITS NOT RAINING ON YOU TODAY....YET :(
+                      </div>
+                    </div>
+                  </div>`;
+         }
+     } else {
+         alert("Sorry, please check the city and state fields and try again");
+     }
+     $('.js-results').html(resultElement);
+ };
 
-// Document.ready equivalent
-$(() => {
-    watchSubmit();
-});
+ // Event listener for 'submit' button
+ const watchSubmit = () => {
+     $('.button').click((e) => {
+         e.preventDefault();
+         console.log('hello');
+         let queryCity = $('.js-query-city').val().replace(/ /g, "_");
+         let queryState = $('.js-query-state').val();
+         getDataFromApi(queryCity, queryState, displayWunderGroundData);
+     });
+ };
+ // window.addEventListener('keydown', watchSubmit);
+ // Document.ready equivalent
+ $(() => {
+     watchSubmit();
+ });
